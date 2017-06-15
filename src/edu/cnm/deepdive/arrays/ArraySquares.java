8b56ea2207f0fc23933812ea02ae72555eebba90
LoneLeather03 <davidmartinez333@gmail.com>
2017-06-15 15:10:58 -0600

    Completed array of squares

---
+++ src/edu/cnm/deepdive/arrays/ArraySquares.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive.arrays;
+
+import java.util.Arrays;
+
+public class ArraySquares {
+
+  public static void main(String[] args) {
+    int[] squares = new int[50];
+    for (int i = 0; i < squares.length; i++) {
+      squares[i] = i * i;
+    }
+    System.out.println(Arrays.toString(squares));
+    
+ 
+
+  }
+
+}
